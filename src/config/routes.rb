5724bf401ddd92605cf76dd504c4f5e5a1d655c0
kojimakei <kojimaruuuuu@gmail.com>
2021-04-13 17:40:12 +0900

    ルーティングの設定

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'ideas/index'
-      get 'ideas/create'
+  namespace 'api' do
+    namespace 'v1' do
+      resources :categories, only: [:create]
+      resources :ideas, only: [:create, :index]
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
