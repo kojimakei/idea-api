da0a0942f667a3206fe5f8d1f0c331253994783f
kojimakei <kojimaruuuuu@gmail.com>
2021-04-13 16:49:09 +0900

    ideaテーブル作成

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_074533) do
+ActiveRecord::Schema.define(version: 2021_04_13_074804) do
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -18,4 +18,13 @@ ActiveRecord::Schema.define(version: 2021_04_13_074533) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "ideas", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_ideas_on_category_id"
+  end
+
+  add_foreign_key "ideas", "categories"
 end
