6bef1d7625967d029eb62cf91bb190f570613ab1
kojimakei <kojimaruuuuu@gmail.com>
2021-04-15 14:37:56 +0900

    テストの実装

--- src/spec/factories/ideas.rb
+++ src/spec/factories/ideas.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :idea do
-    
+    body        {'test'}
+    category_id {2}
+    association :category
   end
 end
--- src/spec/requests/api/v1/ideas_spec.rb
+++ src/spec/requests/api/v1/ideas_spec.rb
@@ -1,18 +1,74 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Ideas", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/ideas/index"
-      expect(response).to have_http_status(:success)
+
+describe Api::V1::IdeasController, type: :request do
+
+
+  # ---------------------アイデア取得API------------------------
+  describe 'GET #index' do
+    it 'category_nameが指定されていない場合は全てのideasを返却' do 
+      @ideas = FactoryBot.create_list(:idea, 10)
+
+      get '/api/v1/ideas'
+      json = JSON.parse(response.body)
+      # リクエスト成功を表す200が返ってきたか確認する。
+      expect(response.status).to eq(200)      
+      expect(json.length).to eq(Idea.count)
+    end
+
+    it 'category_nameが指定されている場合は該当するcategoryのideasの一覧を返却' do 
+      expect do
+        @idea_params = {
+          idea_form: {
+            categoty_name: "category_name"
+          }
+        }
+        post "get '/api/v1/ideas'", params: @idea_params
+        expect(response.status).to eq(200)
+        expect(json.length).to eq(Idea.count)
+      end
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/ideas/create"
-      expect(response).to have_http_status(:success)
+
+  # ---------------------アイデア登録API------------------------
+  describe 'POST #create' do
+    it 'category_nameとbodyがある場合はアイデア登録できる' do
+        @idea_params = {
+          idea_form: {
+            categoty_name: "category_name",
+            body: "category_body"
+          }
+        }
+        expect do
+        post "post '/api/v1/ideas'", params: @idea_params
+        expect(response.status).to eq(201)
+      end.to change {IdeaForm.count}.by(1)
+    end
+
+    it 'category_nameが空の場合はアイデア登録できる' do
+      expect do
+        @idea_params = {
+          idea_form: {
+            categoty_name: "",
+            body: "category_body"
+          }
+        }
+        post "post '/api/v1/ideas'", params: @idea_params
+        expect(response.status).to eq(422)
+      end
+    end
+    it 'bodyが空の場合はアイデア登録できる' do
+      expect do
+        @idea_params = {
+          idea_form: {
+            categoty_name: "category_name",
+            body: ""
+          }
+        }
+        post "post '/api/v1/ideas'", params: @idea_params
+        expect(response.status).to eq(422)
+      end
     end
   end
-
 end
